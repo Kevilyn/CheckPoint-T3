d1357046162ceb8c476ec9126747b3fa4ec35a37
Kevilyn <kevilynqd@hotmail.com>
2022-03-20 17:03:38 -0300

    Menu_Microondas

--- Microondas.js
+++ Microondas.js
@@ -1,103 +1,72 @@
 let Pipoca = 10;
-let Macarrao = 8;
+let Macarrão = 8;
 let Carne = 15;
 let Feijao = 12;
 let Brigadeiro = 8;
 
 //  Função Microondas
 function microondas(opcao, tempo) {
+    //O switch vamos vericar as opções e o tempo
     switch(opcao) {
         case "Pipoca" :
-            if(tempo >= Pipoca * 2 && tempo < Pipoca * 3){          // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-                console.log(`A comida (${opcao}) queimou!`)
-                return `A comida (${opcao}) queimou!`
-            } else if (tempo < Pipoca){                             // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
-                console.log("Tempo insuficiente!")
-                return "Tempo insuficiente"
-            }else if (tempo >= Pipoca * 3){                         // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-                console.log("KABUM!")
-                return "KABUM!"
-            } else if (tempo = Pipoca){
-                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
-                return `Prato (${opcao}) pronto, bom apetite!!!`; 
-             }
+            if (tempo < 10) // Limitando meu tempo menor do que 10 segundos; Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+           return `Tempo insuficiente!`;
+            else if (tempo = 10) // Limitando tempo entre 10 e 19 segundos
+            return `${opcao} pronto(a), bom apetite`;
+            else if (tempo >= 20 && tempo <= 29) // Limitando tempo ente 20 e 29 segundos
+            return `A comida(${opcao}) queimou!`
+            else 
+            return `Kabummm` // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”
 
             break;
-        case "Macarrao" :
-            if(tempo >= Macarrao * 2 && tempo < Macarrao * 3){      // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-                console.log (`A comida (${opcao}) queimou!`)
-                return `A comida (${opcao}) queimou!`
-            } else if (tempo < Macarrao){                          // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
-                console.log("Tempo insuficiente!")
-                return "Tempo insuficiente"
-            }else if (tempo >= Macarrao * 3){                      // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-                console.log("KABUM!")
-                return "KABUM!"
-            } else if (tempo = Macarrao){
-                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
-                return `Prato (${opcao}) pronto, bom apetite!!!`;
-            }
-
+            case "Macarrao" :
+                if (tempo < 8) // Limitando tempo menor do que 8 segundos; Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                return `Tempo insuficiente!`;
+                else if (tempo >= 8 && tempo <= 15) // Limitando tempo entre 8 e 15 segundos
+                return `${opcao} pronto(a), bom apetite!`;
+                else if (tempo >= 16 && tempo <= 23) // Limitando tempo entre 16 e 23 segundos
+                return `A comida queimou!` ;
+                else 
+                return `Kabummm` // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”
             break;
-        case "Carne" :
-            if(tempo >= Carne * 2 && tempo < Carne * 3){            // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-                console.log(`A comida (${opcao}) queimou!`)
-                return `A comida (${opcao}) queimou!`
-            } else if (tempo < Carne){                              // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
-                console.log("Tempo insuficiente!")
-                return "Tempo insuficiente"
-            }else if (tempo >= Carne * 3){                          // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-                console.log("KABUM!")
-                return "KABUM!"
-            } else if (tempo = Carne){
-                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
-                return `Prato (${opcao}) pronto, bom apetite!!!`;
-            }
+            case "Carne" :
+                if (tempo < 15) // Limitando tempo menor do que 15 segundos; Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                return `Tempo insuficiente!`;
+                else if (tempo >= 15 && tempo <= 29) // Limitando tempo entre 15 e 29 segundos
+                return `${opcao} pronto(a), bom apetite!`;
+                else if (tempo >= 30 && tempo <= 44) // Limitando tempo ente 30 e 44 segundos
+                return `A comida(${opcao}) queimou!`;
+                else 
+                return `Kabummm` // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”
             break;
-        case "Feijao" :
-            if(tempo >= Feijao * 2 && tempo < Feijao * 3){          // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-                console.log(`A comida (${opcao}) queimou!`)
-                return `A comida (${opcao}) queimou!`
-            } else if (tempo < Feijao){                             // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
-                console.log("Tempo insuficiente!")
-                return "Tempo insuficiente"
-            }else if (tempo >= Feijao * 3){                         // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-                console.log("KABUM!")
-                return "KABUM!"
-            } else if (tempo = Feijao){
-                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
-                return `Prato (${opcao}) pronto, bom apetite!!!`;
-            }
-
+            case "Feijao" :
+                if (tempo < 12) // Limitando tempo menor do que 12 segundos; Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                return `Tempo insuficiente!`;
+                else if (tempo >= 12 && tempo <= 23) // Limitando tempo entre 12 e 23 segundos
+                return `${opcao} pronto(a), bom apetite!`;
+                else if (tempo >= 24 && tempo <= 35) // Limitando meu tempo ente 24 e 35 segundos
+                return `A comida(${opcao}) queimou!`;
+                else 
+                return `Kabummm` // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”
             break;
-        case "Brigadeiro" :
-            if(tempo >= Brigadeiro * 2 && tempo < Brigadeiro * 3){  // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-                console.log(`A comida (${opcao}) queimou!`)
-                return `A comida (${opcao}) queimou!`
-            } else if (tempo < Brigadeiro){                         // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
-                console.log("Tempo insuficiente!")
-                return "Tempo insuficiente"
-            }else if (tempo >= Brigadeiro * 3){                     // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-                console.log("KABUM!")
-                return "KABUM!"
-            } else if (tempo = Brigadeiro){
-                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
-                return `Prato (${opcao}) pronto, bom apetite!!!`;
-            }
-            
+            case "Brigadeiro" :
+                if (tempo < 8) // Limitando tempo menor do que 10 segundos; Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                return `Tempo insuficiente!`;
+                else if (tempo >= 8 && tempo <= 15) // Limitando tempo entre 10 e 19 segundos
+                return `${comidas} pronto(a), bom apetite!`;
+                else if (tempo >= 16 && tempo <= 23) // Limitando tempo ente 20 e 29 segundos
+                return `A comida(${comidas}) queimou!`;
+                else 
+                return `Kabummm` // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”
             break;
-        default :
-            console.log("Prato inexistente!");  // Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente"
-    }
-}
-
-
-
-
+            default:
+                return `Prato inexistente!` // Opções de comida que não estejam listados no switch; Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente"
+        }
+    } 
     // Opção de comida
-        microondas("Pipoca",15);
-        microondas("Macarrao",7);
-        microondas("Carne",45);
-        microondas("Feijao",30);
-        microondas("Brigadeiro",8);
-        microondas("Miojo",5);
+    console.log((microondas("Miojo",5)));
+    console.log((microondas("Pipoca", 10))); 
+    console.log((microondas("Macarrao", 10)));
+    console.log((microondas("Carne", 30)));
+    console.log((microondas("Feijao", 30)));
+    console.log((microondas("Brigadeiro", 40)));
---
+++ Microondas1.js
@@ -0,0 +1,103 @@
+let Pipoca = 10;
+let Macarrao = 8;
+let Carne = 15;
+let Feijao = 12;
+let Brigadeiro = 8;
+
+//  Função Microondas
+function microondas(opcao, tempo) {
+    switch(opcao) {
+        case "Pipoca" :
+            if(tempo >= Pipoca * 2 && tempo < Pipoca * 3){          // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+                console.log(`A comida (${opcao}) queimou!`)
+                return `A comida (${opcao}) queimou!`
+            } else if (tempo < Pipoca){                             // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                console.log("Tempo insuficiente!")
+                return "Tempo insuficiente"
+            }else if (tempo >= Pipoca * 3){                         // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+                console.log("KABUM!")
+                return "KABUM!"
+            } else if (tempo = Pipoca){
+                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
+                return `Prato (${opcao}) pronto, bom apetite!!!`; 
+             }
+
+            break;
+        case "Macarrao" :
+            if(tempo >= Macarrao * 2 && tempo < Macarrao * 3){      // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+                console.log (`A comida (${opcao}) queimou!`)
+                return `A comida (${opcao}) queimou!`
+            } else if (tempo < Macarrao){                          // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                console.log("Tempo insuficiente!")
+                return "Tempo insuficiente"
+            }else if (tempo >= Macarrao * 3){                      // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+                console.log("KABUM!")
+                return "KABUM!"
+            } else if (tempo = Macarrao){
+                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
+                return `Prato (${opcao}) pronto, bom apetite!!!`;
+            }
+
+            break;
+        case "Carne" :
+            if(tempo >= Carne * 2 && tempo < Carne * 3){            // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+                console.log(`A comida (${opcao}) queimou!`)
+                return `A comida (${opcao}) queimou!`
+            } else if (tempo < Carne){                              // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                console.log("Tempo insuficiente!")
+                return "Tempo insuficiente"
+            }else if (tempo >= Carne * 3){                          // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+                console.log("KABUM!")
+                return "KABUM!"
+            } else if (tempo = Carne){
+                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
+                return `Prato (${opcao}) pronto, bom apetite!!!`;
+            }
+            break;
+        case "Feijao" :
+            if(tempo >= Feijao * 2 && tempo < Feijao * 3){          // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+                console.log(`A comida (${opcao}) queimou!`)
+                return `A comida (${opcao}) queimou!`
+            } else if (tempo < Feijao){                             // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                console.log("Tempo insuficiente!")
+                return "Tempo insuficiente"
+            }else if (tempo >= Feijao * 3){                         // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+                console.log("KABUM!")
+                return "KABUM!"
+            } else if (tempo = Feijao){
+                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
+                return `Prato (${opcao}) pronto, bom apetite!!!`;
+            }
+
+            break;
+        case "Brigadeiro" :
+            if(tempo >= Brigadeiro * 2 && tempo < Brigadeiro * 3){  // Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+                console.log(`A comida (${opcao}) queimou!`)
+                return `A comida (${opcao}) queimou!`
+            } else if (tempo < Brigadeiro){                         // Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"
+                console.log("Tempo insuficiente!")
+                return "Tempo insuficiente"
+            }else if (tempo >= Brigadeiro * 3){                     // Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+                console.log("KABUM!")
+                return "KABUM!"
+            } else if (tempo = Brigadeiro){
+                console.log(`Prato (${opcao}) pronto, bom apetite!!!`);
+                return `Prato (${opcao}) pronto, bom apetite!!!`;
+            }
+            
+            break;
+        default :
+            console.log("Prato inexistente!");  // Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente"
+    }
+}
+
+
+
+
+    // Opção de comida
+        microondas("Pipoca",15);
+        microondas("Macarrao",7);
+        microondas("Carne",45);
+        microondas("Feijao",30);
+        microondas("Brigadeiro",8);
+        microondas("Miojo",5);
